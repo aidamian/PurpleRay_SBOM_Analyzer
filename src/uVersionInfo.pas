fe035eec9abb971a3e8c82351374eca445e75c23
Andrei Ionut Damian <andrei.damian@me.com>
2026-08-23 21:12:50 +0300

    fix: update ver

--- src/uVersionInfo.pas
+++ src/uVersionInfo.pas
@@ -29,7 +29,7 @@ interface
 const
   AppName = 'PurpleRay SBOM Analyzer';
   AppExecutableName = 'purpleray-sbom-analyzer';
-  AppVersion = '0.9.0';
+  AppVersion = '0.9.2';
   AppCommit = 'unknown';
 
 {**
